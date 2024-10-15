7455a0760ca2bf1d3dd1bb14ebbcf3b4ffd133d5
gabrielramos02 <gabrielramos02ok@outlook.com>
2024-10-15 19:09:56 -0400

    reordering folders & first changes

--- telegramBot.py
+++ telegramBot.py
@@ -1,44 +1,50 @@
+from pydoc import resolve
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from os import environ
-
+from commands.start import start_command
+from commands.help import help_command
+from responses.handle_magnet import handle_magnet
+from responses.handle_url import handle_url
+import re
 
 TOKEN = environ["TOKEN"]
 BOT_USERNAME = environ["BOT_USERNAME"]
-# Commands
-
-async def start_command( update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello World")
-
-async def help_command( update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Help Command")
 
 # Responses
 # aqui revisa si es una url o un magnet
-def handle_response(text: str) -> str:
-    return text
-
-# logica de los mensajes
-async def handle_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
     text: str = update.message.text
+    magnet_regex = "^magnet:"
+    #url_regex = "https?:\/\/(www\.)?[a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)?"
+
+    magnet_match = re.search(string=text,pattern=magnet_regex)
+    #url_match = re.search(string=text,pattern=url_regex)
 
     print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
 
     if message_type == 'group':
-        if BOT_USERNAME in text:
-            response = "group"
-        else: return
+        return
     else:
-        response = "chat"
+        if magnet_match:
+            response = handle_magnet(text)
+        #elif url_match:
+            #response = handle_url(text)
+        else: 
+            response = "URL con formato incorrecto"
     await update.message.reply_text(response)
 
+
+#App Initial
 if __name__ in "__main__":
     print("Starting Bot")
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start",start_command))
 
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
     print("Polling")
-    app.run_polling(poll_interval=10)
+    app.run_polling(poll_interval=3)
 
     
